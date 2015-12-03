05b6d5c318c3a2d50c39f776528f2f66fd04d0e4
Julien Custoja <julien.custoja@gmail.com>
2015-12-03 20:35:07 +0100

    Ajout + affichage de post

--- app/controllers/Civil.java
+++ app/controllers/Civil.java
@@ -1,5 +1,12 @@
 package controllers;
 
+import javafx.geometry.Pos;
+import models.Location;
+import models.Post;
+import models.TypePost;
+
+import java.util.List;
+
 /**
  * Created by Elliot on 03/12/2015.
  */
@@ -10,7 +17,24 @@ public class Civil extends BaseController {
     }
 
     public static void dashboard() {
-        render();
+        List<Post> lesPosts = Post.all().fetch();
+        render(lesPosts);
+    }
+
+    public static void ajouterPost(String post, Double lat, Double lng) {
+        Post p = new Post();
+        p.text = post;
+        p.lat = lat;
+        p.lng = lng;
+        p.typePost = TypePost.OK;
+        p.save();
+        flash.success("Votre post a été pris en compte");
+        dashboard();
+    }
+
+
+    public static ajouterPostDanger() {
+
     }
 
 }
