214cbddecad34273e6ed1dee9feca7138a1c7504
manantsoa <manantsoa.razana@gmail.com>
2015-12-03 23:54:07 +0100

    Ajout d'un attribut YoutubeURL et modification du dashboard

--- app/controllers/Civil.java
+++ app/controllers/Civil.java
@@ -24,7 +24,7 @@ public class Civil extends BaseController {
         render(lesPosts, demandeSecours, lesGroupes, nbPosts);
     }
 
-    public static void ajouterPost(String post, Double lat, Double lng) {
+    public static void ajouterPost(String post, Double lat, Double lng, String youtubeURL) {
 
         String tag = null;
         List<String> splitStr = Arrays.asList(post.split("\\s+"));
@@ -37,6 +37,7 @@ public class Civil extends BaseController {
 
         Post p = new Post();
         p.text = post;
+        p.youtubeURL = youtubeURL;
         p.lat = lat;
         p.lng = lng;
         p.tag = tag;
