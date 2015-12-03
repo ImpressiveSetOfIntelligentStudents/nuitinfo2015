9a5248ab138a6cc51e83782a8a80bda587978fbe
superkebab <felgines.elliot@gmail.com>
2015-12-03 20:21:35 +0100

    Création des vues index et dashboard (front)

---
+++ app/controllers/Civil.java
@@ -0,0 +1,16 @@
+package controllers;
+
+/**
+ * Created by Elliot on 03/12/2015.
+ */
+public class Civil extends BaseController {
+
+    public static void index() {
+        render();
+    }
+
+    public static void dashboard() {
+        render();
+    }
+
+}
