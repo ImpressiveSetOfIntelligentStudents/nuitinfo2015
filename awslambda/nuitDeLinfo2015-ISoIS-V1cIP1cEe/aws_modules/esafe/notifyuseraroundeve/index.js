d8d260b4d7ac372628af76974d27ece980e4c0bf
Benoît Sauvère <benoit.sauvere@teevity.com>
2015-12-04 06:50:26 +0100

    Ajout d'une nouvelle lambda qui récupère la liste de tout les utilisateurs de la base avec leur coordonnées. La lambda d'envoi d'email rechercher dans la base de données les utilisateur dans un rayon de 10km.

--- awslambda/nuitDeLinfo2015-ISoIS-V1cIP1cEe/aws_modules/esafe/notifyuseraroundeve/index.js
+++ awslambda/nuitDeLinfo2015-ISoIS-V1cIP1cEe/aws_modules/esafe/notifyuseraroundeve/index.js
@@ -2,19 +2,84 @@
  * AWS Module: Action: Modularized Code
  */
 
+var AWS = require('aws-sdk');
+
+var toRadians = function(v) {
+  return v * Math.PI / 180;
+}
+
+var getBoundingBox = function(latitude, longitude, distance) {
+
+  var latRadian = toRadians(latitude);
+
+  var degLatKm = 110.574235;
+  var degLongKm = 110.574235 * Math.cos(latRadian);
+  var deltaLat = distance / 1000.0 / degLatKm;
+  var deltaLong = distance / 1000.0 / degLongKm;
+
+  var minLat = latitude - deltaLat;
+  var minLong = longitude - deltaLong;
+  var maxLat = latitude + deltaLat;
+  var maxLong = longitude + deltaLong;
+
+  return {
+    min: {
+      lat: minLat,
+      long: minLong
+    },
+    max : {
+      lat: maxLat,
+      long: maxLong
+    }
+  }
+
+}
+
+
+
+
 // Export For Lambda Handler
 module.exports.run = function(event, context, cb) {
-  return cb(null, action(event, context));
+
+  var sourceUser = event.sourceUser;
+  var latitude = event.latitude;
+  var longitude = event.longitude;
+
+  var dynamodb = new AWS.DynamoDB();
+
+  // Calcul des bornes des coordonnées pour un rayon de 10km
+  var boundingBox = getBoundingBox(latitude, longitude, 10)
+
+  var requestparams = {
+    TableName: "Esafe-Userdatabasw",
+    KeyConditionExpression: "latitude between '" + boundingBox.min.lat + "' and '" + boundingBox.max.lat  + "' "
+                              + " and longitude between '" + boundingBox.min.long + "' and '" + boundingBox.max.long + "'"
+  }
+
+  dynamodb.query(requestparams, function (err, data) {
+    if (err) {
+      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+
+    } else {
+      console.log("Query succeeded.");
+      data.Items.forEach(function(item) {
+        console.log(" -", item.year + ": " + item.title);
+      });
+    }
+
+
+    // TODO: Envoyer un email au users
+
+    cb(null, {"data": data, "err": err});
+  });
 };
 
 // Your Code
 var action = function(event, context) {
 
-//  var sourceUser = event.sourceUser;
-  //var latitude = event.latitude;
-  //var longitude = event.longitude;
+
 
   //return {message: 'Event received from ' + sourceUser + ' for latitude: ' + latitude + ' longitude:' + longitude};
 
-  return {"message": JSON.stringify(event)}
+  return {"eventData": JSON.stringify(event)}
 };
